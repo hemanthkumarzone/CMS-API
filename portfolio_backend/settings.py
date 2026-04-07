42d3184f10a1609ab887512e8d860d6a55d1b610
veeratnj <veerakumarhgkfm@gmail.com>
2026-04-07 15:14:00 +0530

    cors

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8kizvd*m7xopj=g!-#3s)^dfo+z-n7z3b18xtkt%r+v=iz-m-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -53,7 +53,28 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     
 ]
+
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 ROOT_URLCONF = 'portfolio_backend.urls'
 
