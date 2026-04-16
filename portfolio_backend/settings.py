2ae4ade7add3679db2a9fdd0e1aa18aeae68d764
Chejarladhanu18 <chejarladhanalakshmi18@gmail.com>
2026-04-16 13:47:35 +0530

    added welcoming mail

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'CtrlS',
     'rest_framework',
     'corsheaders',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# ================= EMAIL CONFIG =================
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'chejarladhanalakshmi18@gmail.com'
+EMAIL_HOST_PASSWORD = 'zmahyhhppmeztkdg'
